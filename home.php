42315fa21b3460c540e0a5c3a8ad53ae28a0aec9
Rahul Dutta <rdtech2002@gmail.com>
2022-06-02 19:00:06 +0530

    ..

--- home.php
+++ home.php
@@ -2,67 +2,64 @@
 include 'admin/db_connect.php'; 
 ?>
 <style>
-#portfolio .img-fluid{
-    width: calc(100%);
-    height: 30vh;
-    z-index: -1;
-    position: relative;
-    padding: 1em;
-}
-.event-list{
-cursor: pointer;
-}
-span.hightlight{
-    background: yellow;
-}
-.banner{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        min-height: 26vh;
-        width: calc(30%);
-    }
-    .banner img{
+    #portfolio .img-fluid{
         width: calc(100%);
-        height: calc(100%);
-        cursor :pointer;
+        height: 30vh;
+        z-index: -1;
+        position: relative;
+        padding: 1em;
     }
-.event-list{
-cursor: pointer;
-border: unset;
-flex-direction: inherit;
-}
+    .event-list{
+    cursor: pointer;
+    }
+    span.hightlight{
+        background: yellow;
+    }
+    .banner{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 26vh;
+            width: calc(30%);
+        }
+        .banner img{
+            width: calc(100%);
+            height: calc(100%);
+            cursor :pointer;
+        }
+    .event-list{
+    cursor: pointer;
+    border: unset;
+    flex-direction: inherit;
+    }
 
-.event-list .banner {
-    width: calc(40%)
-}
-.event-list .card-body {
-    width: calc(60%)
-}
-.event-list .banner img {
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
-    min-height: 50vh;
-}
-span.hightlight{
-    background: yellow;
-}
-.banner{
-   min-height: calc(100%)
-}
+    .event-list .banner {
+        width: calc(40%)
+    }
+    .event-list .card-body {
+        width: calc(60%)
+    }
+    .event-list .banner img {
+        border-top-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+        min-height: 50vh;
+    }
+    span.hightlight{
+        background: yellow;
+    }
+    .banner{
+    min-height: calc(100%)
+    }
 </style>
+
         <header class="masthead">
             <div class="container-fluid h-100">
                 <div class="row h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-8 align-self-end mb-4 page-title">
                     	<h3 class="text-white">Welcome to <?php echo $_SESSION['system']['name']; ?></h3>
                         <hr class="divider my-4" />
-
                     <div class="col-md-12 mb-2 justify-content-center">
-                    </div>                        
-                    </div>
-                    
-                </div>
+                </div>                        
             </div>
         </header>
             <div class="container mt-3 pt-2">
